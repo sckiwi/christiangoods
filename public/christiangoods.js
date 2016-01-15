94959e203aa2de41598ada8700bec518c2a0b8e2
S Clark <spence@Ss-MacBook-Air.local>
2016-01-15 12:58:22 -0700

    trial

--- public/christiangoods.js
+++ public/christiangoods.js
@@ -4,11 +4,13 @@ var app = angular.module('christiangoods', ['firebase','ngMaterial']);
 
 app.controller('main', function($scope, $firebaseObject) {
 
-  // var firebaseRef = new Firebase("https://colorwars.firebaseio.com/");
-  // var firebaseObj = $firebaseObject(firebaseRef);
-  // firebaseObj.$bindTo($scope, "bitMap").then(function(){
-  //   //This puts the list of square bitIds in the right variable for the ng-repeat, but not until after the data is retrieved
-  //   $scope.bitIds = $scope.bitIdsSource;
-  // });
+  $scope.products = {};
+
+  var firebaseRef = new Firebase("https://christiangoods.firebaseio.com/products");
+  var firebaseObj = $firebaseObject(firebaseRef);
+  firebaseObj.$bindTo($scope, "products").then(function(){
+    // run code after the products come back from Firebase
+    // Nothing to do right now
+  });
 
 });
