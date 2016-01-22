92c312f64da7b3ed6334b9c7e67fb08629f674ee
S Clark <spence@Ss-MacBook-Air.local>
2016-01-22 12:43:21 -0700

    Nice

--- public/christiangoods.js
+++ public/christiangoods.js
@@ -4,6 +4,7 @@ var app = angular.module('christiangoods', ['firebase','ngMaterial']);
 
 app.controller('main', function($scope, $firebaseObject) {
 
+  $scope.shopperEmail = "spencergclark@gmail.com";
   $scope.products = {};
 
   var firebaseRef = new Firebase("https://christiangoods.firebaseio.com/products");
@@ -12,5 +13,37 @@ app.controller('main', function($scope, $firebaseObject) {
     // run code after the products come back from Firebase
     // Nothing to do right now
   });
+
+  $scope.likesCount = function(product) {
+    return (product.likes)?Object.keys(product.likes).length:0;
+  };
+
+  $scope.getProductImageStyle = function(product) {
+    return {
+      "background-image": "url('" + product.imageUrl + "')"
+    };
+  };
+
+  $scope.mikeLikesIt = function(product) {
+    if (product.likes) {
+      for (var i = 0; i < Object.keys(product.likes).length; i++) {
+        var likeEmail = product.likes[Object.keys(product.likes)[i]];
+        if (likeEmail.toLowerCase() == $scope.shopperEmail.toLowerCase()) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+  $scope.likeButtonClicked = function(product) {
+    if ($scope.mikeLikesIt(product)) {
+      // Remove $scope.shopperEmail from the likes Object
+      alert('TODO: we sshill need to remove this like');
+    } else {
+      // Add $scope.shopperEmail to the likes Object
+      alert('TODO: We still need to add this like');
+    }
+  }
 
 });
